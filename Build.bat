c0b08c4ea5b96cf16520f89025c9f5e932a515a7
Guillaume Plante <guillaume.plante@luminator.com>
2024-05-22 00:58:45 -0400

    auto

--- Build.bat
+++ Build.bat
@@ -8,15 +8,16 @@ setlocal EnableDelayedExpansion
 ::      Build different configuration of the app
 ::
 :: ==============================================================================
-::   arsccriptum - made in quebec 2020 <guillaumeplante.qc@gmail.com>
+::   Guilaume Plante <guillaume.plante@luminator.com>
+::   Copyright (C) Luminator Technology Group
 :: ==============================================================================
 
 goto :init
 
 :init
-    call :validate_build_automation
     set "__scripts_root=%AutomationScriptsRoot%"
-    call :read_script_root development\build-automation  BuildAutomation
+    set "__executable_name=AesCrypter.exe"
+    call :read_script_data development\build-automation  BuildAutomation
     set "__script_file=%~0"
     set "__target=%~1"
     set "__script_path=%~dp0"
@@ -25,52 +26,31 @@ goto :init
     set "__lib_out=%__scripts_root%\batlibs\out.bat"
     ::*** This is the important line ***
     set "__build_cfg=%__script_path%buildcfg.ini"
-    set "__bin_path=%__script_path%bin"
-    set "__tmp_path=%__script_path%vs\___temp_compilation_files"
     set "__build_cancelled=0"
     goto :validate
-
-:validate_build_automation
-    if not defined OrganizationHKCU       call :header_err  && goto :eof
-    if not defined AutomationScriptsRoot  call :header_err  && goto :eof
-    if not exist %AutomationScriptsRoot%  call :header_err  && goto :eof
-
-    goto :eof
-
 
 
 :header
     echo. %__script_name% v%__script_version%
-    echo.    This script is part of arsscriptum build wrappers.
+    echo.    This script is part of my custom LTG build wrappers.
     echo.
     goto :eof
 
 :header_err
-    echo.**************************************************
-    echo.This script is part of arsscriptum build wrappers.
-    echo.**************************************************
+    echo. =======================================================
+    echo. = This script is part of my custom LTG build wrappers =
+    echo. =======================================================
     echo.
-    echo. YOU NEED TO HAVE THE BuildAutomation Scripts setup on you system...
-    echo. https://github.com/arsscriptum/BuildAutomation
+    echo. YOU NEED TO HAVE THE BuildAutomation Scripts setup
+    echo. on you system...
+    echo. http://vsvr-gitlab-01/guillaume.plante/buildautomation
     goto :eof
 
 
-
-:clean_tmp
-    call %__lib_out% :__out_l_red " Cleaning : %__tmp_path%"  
-    echo.
-    rmdir "%__tmp_path%" /S /Q
-    goto :eof
-
-:clean_bin
-    call %__lib_out% :__out_l_red " Cleaning : %clean_bin%"  
-    echo.
-    :: rmdir "P:\Development\IrfanGet\bin" /S /Q
-    rmdir "%__bin_path%" /S /Q
-    goto :eof
-
-:read_script_root
+:read_script_data
+    if not defined OrganizationHKCU          call :header_err && call :error_missing_path OrganizationHKCU::= & goto :eof
     set regpath=%OrganizationHKCU::=%
+    ::echo "OrganizationHKCU => %regpath%"
     for /f "tokens=2,*" %%A in ('REG.exe query %regpath%\%1 /v %2') do (
             set "__scripts_root=%%B"
         )
@@ -88,9 +68,9 @@ goto :init
 
 :prebuild_header
     call %__lib_date% :getbuilddate
-    call %__lib_out% :__out_d_red " ======================================================================="
-    call %__lib_out% :__out_l_red " Compilation started for %cd%  %__target%"  
-    call %__lib_out% :__out_d_red " ======================================================================="
+    call %__lib_out% :__out_d_yel "======================================================="
+    call %__lib_out% :__out_d_red "=        AESCRYPTER - STARTING COMPILATION            ="
+    call %__lib_out% :__out_d_yel "======================================================="
     call :build
     goto :eof
 
@@ -101,59 +81,65 @@ goto :init
 :call_make_build
     set config=%1
     set platform=%2
-    call %__makefile% /v /i %__build_cfg% /t Build /c %config% /p %platform%
-    goto :finished
+    call %__makefile% /i %__build_cfg% /t Build /c %config% /p %platform%
+    goto :end
 
 :call_make_build_export
     set config=%1
     set platform=%2
-    set export_path=%3
-    call %__makefile% /v /i %__build_cfg% /t Build /c %config% /p %platform% /x %export_path%
-    goto :finished
+    call %__makefile% /i %__build_cfg% /t Build /c %config% /p %platform% /x
+    goto :end
 
 :: ==============================================================================
 ::   Build static
 :: ==============================================================================
-:build_x86
+:build_x86_debug
     call :call_make_build Debug x86
-    call :call_make_build Release x86
-    goto :eof
+    goto :end
 
 :: ==============================================================================
 ::   Build x64
 :: ==============================================================================
-:build_x64
-    call :call_make_build Release x64
-    goto :eof
+:build_Release
+    call :call_make_build_export Release x64
+    goto :end
 
 :: ==============================================================================
 ::   clean all
 :: ==============================================================================
 :clean
-    call %__makefile% /v /i %__build_cfg% /t Clean /c Debug /p x86
-    call %__makefile% /v /i %__build_cfg% /t Clean /c Release /p x86
-    call %__makefile% /v /i %__build_cfg% /t Clean /c Debug /p x64
-    call %__makefile% /v /i %__build_cfg% /t Clean /c Release /p x64
-    goto :eof
+    call %__makefile% /i %__build_cfg% /t Clean /c Debug /p x86
+    call %__makefile% /i %__build_cfg% /t Clean /c Release /p x86
+    call %__makefile% /i %__build_cfg% /t Clean /c Debug /p x64
+    call %__makefile% /i %__build_cfg% /t Clean /c Release /p x64
+    goto :end
+
+
+:protek
+	set APP_PATH=%cd%\bin\x64\Release
+	call %__lib_out% :__out_n_l_yel "ENCRYPTION"
+	"%AXPROTECTOR_SDK%\bin\AxProtector.exe" -x -kcm -f6000010 -p101001 -cf0 -d:6.20 -fw:3.00 -slw -nn -cav -cas100 -wu1000 -we100 -eac -eec -eusc1 -emc -v -cag23 -caa7 -o:"%APP_PATH%\%__executable_name%" "%APP_PATH%\%__executable_name%"
+	call %__lib_out% :__out_d_grn " SUCCESS"
+	goto :end
 
 
 :: ==============================================================================
 ::   Build
 :: ==============================================================================
 :build
-    call :clean_bin
-    call :clean_tmp
-	
 	if "%__target%" == "clean" (
 		call :clean
-		goto :finished
 		)
     if "%__target%" == "rebuild" (
 		call :clean
 		)
-    
+    if "%__target%" == "debug" (
+        call :build_x86_debug
+        )
     ::call :build_x86
-    call :build_x64
+
+    call :build_Release
+    ::call :protek
     goto :finished
 
 
@@ -176,4 +162,11 @@ goto :init
 
 
 :finished
-    call %__lib_out% :__out_d_grn "Build complete"
+    call %__lib_out% :__out_log_script_success %__script_file%
+    exit /B
+
+:end
+    exit /B
+
+
+ 
---
+++ Clean.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: ==============================================================================
+:: 
+::   clean.bat
+:: 
+:: ==============================================================================
+::   Guilaume Plante <guillaume.plante@luminator.com>
+::   Copyright (C) Luminator Technology Group
+:: ==============================================================================
+
+setlocal
+
+rem ## Run script
+call Build.bat Clean
